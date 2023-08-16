fa858bfa10efb50995e836328268bf5ea9b1f0d3
mironovisa <mironovisa@gmail.com>
2023-08-16 12:01:32 +0300

    Added mongo

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require("express");
+const connectDB = require("./config/db");
+
 const app = express();
 const port = process.env.PORT || 3000;
 
 // Set up middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Connect to MongoDB
+connectDB();
 
 // Define a route
 app.get("/", (req, res) => {
