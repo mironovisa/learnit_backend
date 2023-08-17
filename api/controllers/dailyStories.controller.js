a1fa7a0c4ba177c1787569956b22d1b38a3d6eba
mironovisa <mironovisa@gmail.com>
2023-08-17 17:43:00 +0300

    Working daily story API

--- api/controllers/dailyStories.controller.js
+++ api/controllers/dailyStories.controller.js
@@ -6,9 +6,14 @@ const getDailyStoriesForToday = async (req, res) => {
 
     const latestStory = await DailyStories.findOne().sort({ createdAt: -1 });
 
-    console.log("Latest Daily Story:", latestStory);
+    if (!latestStory) {
+      console.log("No daily stories found.");
+      return res.status(404).json({ error: "No daily stories found." });
+    }
 
-    res.json(latestStory);
+    console.log("Latest Daily Story:", latestStory.toJSON());
+
+    res.status(200).json(latestStory.toJSON());
   } catch (error) {
     console.error("Error fetching latest daily story:", error);
     res.status(500).json({ error: "Internal server error" });
--- api/routes/datagen.route.js
+++ api/routes/datagen.route.js
@@ -12,23 +12,6 @@ router.get("/generatedailytext", async (req, res) => {
     res.status(500).json({ error: "An error occurred" });
   }
 });
-
-router.get("/oneForToday", async (req, res) => {
-  // try {
-  //   const dailyStory = await getDailyStoriesForToday(req, res); // Pass req and res to the function
-  //   res.json(dailyStory);
-  // } catch (error) {
-  //   res.status(500).json({ error: "An error occurred" });
-  // }
-  try {
-    console.log("Reached getLatestDailyStory function");
-    const latestStory = await getDailyStoriesForToday();
-    console.log("Latest Daily Story:", latestStory);
-    res.status(200).json(latestStory);
-  } catch (error) {
-    console.error("Error fetching latest daily story:", error);
-    res.status(500).json({ error: "Internal server error" });
-  }
-});
+router.get("/onefortoday", getDailyStoriesForToday);
 
 module.exports = router;
